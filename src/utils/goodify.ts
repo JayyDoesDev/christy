72126c8a0cbf8c788345beb6fabe20524863dd1b
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-10-28 13:59:20 -0400

    chore: goodify change and gitignore

--- src/utils/goodify.ts
+++ src/utils/goodify.ts
@@ -1,3 +1,7 @@
 export function goodify(goodie: string): string {
-  goodie === "present" ? "<:present:1165862478018773013> presen" : "<:candy:1165849590415753287> candy";
+  if (goodie === "present") {
+    return "<:present:1165862478018773013> present";
+  } else if (goodie === "candy") {
+    return "<:candy:1165849590415753287> candy";
+  }
 }
