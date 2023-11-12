b4ff0a022f90e11282f5f49b258984aea06abd38
Eric Pan <75194970+EricPanDev@users.noreply.github.com>
2023-11-11 21:47:47 -0800

    Delete src directory

--- src/utils/goodify.ts
+++
@@ -1,7 +0,0 @@
-export function goodify(goodie: string): string {
-  let string: string = "";
-  goodie === "present"
-    ? string = "<:present:1165862478018773013> present"
-    : string = "<:candy:1165849590415753287> candy";
-    return string;
-}
