691ee05a97bcf85faf4742a3ece36d6daf0e6af2
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-11-13 22:53:57 -0500

    Missing types and simplication added

--- src/core/commands/generic/AboutCommand.ts
+++ src/core/commands/generic/AboutCommand.ts
@@ -1,18 +1,17 @@
-import { Interaction } from "discord.js";
+import { ButtonStyle, ComponentType, Interaction } from "discord.js";
 import { Command } from "../../../structures/Command";
 import { Context } from "../../../structures/Context";
 import { MessageUtil } from "../../../utils/MessageUtil";
-
 import {
   ApplicationCommandType,
   ApplicationCommandOptionType,
 } from "@antibot/interactions";
 
 export default class AboutCommand extends Command {
-  constructor(ctx) {
+  constructor(ctx: Context) {
     super(ctx, {
       name: "about",
-      description: "About the bot",
+      description: "About the bot!",
       interaction: {
         name: "about",
         type: ApplicationCommandType.CHAT_INPUT,
@@ -22,37 +21,38 @@ export default class AboutCommand extends Command {
     });
   }
 
-  async onInteraction(interaction) {
+  async onInteraction(interaction: Interaction): Promise<void> {
     if (!interaction.isCommand()) {
       return;
     }
 
-    const embed = {
-      color: 0x7289DA,
-      title: "About the Bot",
-      description: "Hello there! I'm Marie, your festive companion, spreading holiday cheer with a touch of magic.\n\n🎄✨ I'm here to infuse your holiday season with joy through a collection of delightful commands.\n\nLet the merriment begin! 🌟",
-      footer: {
-        text: MessageUtil.Translate("footer")
-      },
-      thumbnail: {
-        url: "https://cdn.discordapp.com/avatars/802438374240550942/7273f5de1650181b4434d7ba3654706f.webp?size=160",
-      },
-    };
-
-    const components = [
-      {
-        type: 1,
-        components: [
-          {
-            type: 2,
-            style: 5,
-            label: "Github",
-            url: "https://www.github.com/JayyDoesDev/christy",
+    interaction.reply({
+      embeds: [
+        {
+          color: 0x7289da,
+          title: "About the Bot",
+          description: MessageUtil.Translate("cmds.about.aboutDescription"),
+          footer: {
+            text: MessageUtil.Translate("footer"),
           },
-        ],
-      },
-    ];
-
-    interaction.reply({ embeds: [embed], components: components });
+          thumbnail: {
+            url: interaction.user.displayAvatarURL(),
+          },
+        },
+      ],
+      components: [
+        {
+          type: ComponentType.ActionRow,
+          components: [
+            {
+              type: ComponentType.Button,
+              style: ButtonStyle.Link,
+              label: "Github",
+              url: "https://www.github.com/JayyDoesDev/christy",
+            },
+          ],
+        },
+      ],
+    });
   }
 }
